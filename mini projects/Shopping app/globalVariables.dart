0834b3bfe2667857dd794ffa60de412e5095926f
vignesh-newcoder <vigneshdeva5432@gmail.com>
2025-09-16 21:20:22 +0530

    Create globalVariables.dart

---
+++ mini projects/Shopping app/globalVariables.dart
@@ -0,0 +1,34 @@
+final contents = [
+  {
+    'id': 01,
+    'title': 'Men\'s Nikes Shoes',
+    'price': 44.44,
+    'sizes': [5, 6, 7, 8, 9, 10],
+    'imagesUrl': 'assets/images/images_01.png',
+    'company': 'Nike',
+  },
+  {
+    'id': 02,
+    'title': 'Men\'s Puma Shoes',
+    'price': 78.87,
+    'sizes': [7, 8, 9, 10],
+    'imagesUrl': 'assets/images/images_02.png',
+    'company': 'Puma',
+  },
+  {
+    'id': 03,
+    'title': 'Men\'s Ascis Shoes',
+    'price': 63.45,
+    'sizes': [8, 9, 10],
+    'imagesUrl': 'assets/images/images_03.png',
+    'company': 'Ascis',
+  },
+  {
+    'id': 04,
+    'title': 'Men\'s Addidas Shoes',
+    'price': 45.63,
+    'sizes': [7, 8, 9, 10],
+    'imagesUrl': 'assets/images/images_04.png',
+    'compay': 'Addidas',
+  },
+];
