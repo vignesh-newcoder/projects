18aeb18957ba972e79d3d938d244d2d8b3501475
vignesh-newcoder <vigneshdeva5432@gmail.com>
2025-09-22 15:38:01 +0530

    Create main.dart

---
+++ mini projects/CalculatorApp/main.dart
@@ -0,0 +1,26 @@
+import 'package:calculator/pages/Calculation.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Calculator());
+}
+
+class Calculator extends StatefulWidget {
+  const Calculator({super.key});
+
+  @override
+  State<Calculator> createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.system,
+      home: Calculation(),
+    );
+  }
+}
